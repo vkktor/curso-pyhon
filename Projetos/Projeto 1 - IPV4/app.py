dd272592b7ebc8920693fe6fd20cc47c63077cdb
Victor Cardoso Silva <victor.cs@usp.br>
2022-07-21 22:35:31 -0300

    Add files via upload

---
+++ Projetos/Projeto 1 - IPV4/app.py
@@ -0,0 +1,10 @@
+from Classes.calcipv4 import CalcIPv4
+
+calc_ipv4 = CalcIPv4(ip='192.168.0.1', prefixo=30)
+
+print(f'IP: {calc_ipv4.ip}')
+print(f'Máscara: {calc_ipv4.mascara}')
+print(f'Rede: {calc_ipv4.rede}')
+print(f'Broadcast: {calc_ipv4.broadcast}')
+print(f'Prefixo: {calc_ipv4.prefixo}')
+print(f'Número de IPs da Rede: {calc_ipv4.num_ips}')
